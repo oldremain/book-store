5eba85065d9400fe501256e9ddbc3fbb7c0353e1
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-05-22 23:57:26 +0300

    add react-responsive package, change animation styles

--- src/components/main/bookPage/bookDetails/BookDetails.tsx
+++ src/components/main/bookPage/bookDetails/BookDetails.tsx
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
 
 import UIBookRates, { IBookRatesProps } from "../../../UI/bookRates/UIBookRates";
+import UIButton from "../../../UI/button/UIButton";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 import { UISize } from "../../../../enums/enums";
+import { variants } from "./motionVariants";
 
 import s from "./BookDetails.module.scss";
-import UIButton from "../../../UI/button/UIButton";
 
 interface IBookDetailsProps extends IBookRatesProps {}
 
 const BookDetails: React.FC<IBookDetailsProps> = ({ price, isbn }) => {
     const [open, setOpen] = useState<boolean>(false);
+    const isMobile = useMediaQuery({ query: "(max-width: 472px)" });
 
     const handleClick = () => setOpen((prev) => !prev);
+
+    ///const detailsContent = Object.entries()
 
     const RenderedIcon = open ? (
         <KeyboardArrowUpIcon fontSize="small" />
@@ -44,7 +49,8 @@ const BookDetails: React.FC<IBookDetailsProps> = ({ price, isbn }) => {
                     {open && (
                         <motion.ul
                             className={s.more_details_list}
-                            animate={{ y: ["-50%", "0%"], opacity: [0, 1] }}
+                            variants={variants}
+                            animate={isMobile ? "mobile" : "desktop"}
                         >
                             <li className={s.list_item}>isbn 10 :</li>
                             <li className={s.list_item}> 1617294136</li>
@@ -54,8 +60,8 @@ const BookDetails: React.FC<IBookDetailsProps> = ({ price, isbn }) => {
                             <li className={s.list_item}>384</li>
                         </motion.ul>
                     )}
-                    <UIButton text="order" />
                 </div>
+                <UIButton text="Add to cart" />
             </div>
         </div>
     );
---
+++ src/components/main/bookPage/bookDetails/motionVariants.tsx
@@ -0,0 +1,4 @@
+export const variants = {
+    desktop: { y: ["-50%", "0%"], opacity: [0, 1] },
+    mobile: { y: ["-15%", "0%"], opacity: [0, 1] },
+};
