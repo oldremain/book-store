ff7602e9d8e465fc8a64582dbdffdf1acb0ed3df
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-06-10 14:43:02 +0300

    update variables in UiBookImage component

--- src/components/UI/bookImage/UIBookImage.tsx
+++ src/components/UI/bookImage/UIBookImage.tsx
@@ -12,22 +12,22 @@ export interface IBookImageProps {
     cName?: string
 }
 
-const UiBookImage: React.FC<IBookImageProps> = ({ size, theme, image, cName = '' }) => {
+const UiBookImage: React.FC<IBookImageProps> = ({ size, theme, image: src, cName = '' }) => {
     const [loaded, setLoaded] = useState(false)
 
     useEffect(() => {
         const img = new Image()
-        img.src = image
+        img.src = src
         img.onload = () => {
             setLoaded(true)
         }
-    }, [image])
+    }, [src])
 
     return (
         <div className={cn(s.book_image, s[`image_${size}`], s[`bg_${theme}`], s[cName])}>
             {loaded 
                 ? 
-                    <img src={image} alt="it-book-image" /> 
+                    <img src={src} alt="it-book-image" /> 
                 : 
                     <div className={s.image_placeholder}></div>
             }
