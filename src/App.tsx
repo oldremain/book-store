42811786ba5d6c555b3b9e70e0b603bc31b69ae3
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-10-18 12:40:53 +0300

    refactor: change route & add book to cart logic for testing purposes

--- src/App.tsx
+++ src/App.tsx
@@ -17,44 +17,46 @@ import Account from "./components/account/Account";
 import s from "./App.module.scss";
 
 const App: React.FC = () => {
-    const showSidebar = useAppSelector((state) => state.sidebar.show);
-    const isLogged = useAppSelector(state => state.auth.isLogged)
+  const showSidebar = useAppSelector((state) => state.sidebar.show);
+  const isLogged = useAppSelector((state) => state.auth.isLogged);
 
-    const bodyRef = useRef<HTMLBodyElement | null>(null)
+  const bodyRef = useRef<HTMLBodyElement | null>(null);
 
-    useEffect(() => {
-        bodyRef.current = document.querySelector('body')
-        if (bodyRef.current) {
-            bodyRef.current.style.overflow = showSidebar ? 'hidden' : ''
-        }
-    }, [showSidebar])
+  useEffect(() => {
+    bodyRef.current = document.querySelector("body");
+    if (bodyRef.current) {
+      bodyRef.current.style.overflow = showSidebar ? "hidden" : "";
+    }
+  }, [showSidebar]);
 
-    return (
-        <BrowserRouter>
-            <div className={s.app_container}>
-                <Header />
+  return (
+    <BrowserRouter>
+      <div className={s.app_container}>
+        <Header />
 
-                {showSidebar && <Sidebar />}
+        {showSidebar && <Sidebar />}
 
-                <Main>
-                    <Routes>
-                        <Route path="/" element={<BooksPage />}>
-                            <Route index element={<InitialPage />} />
-                            <Route path="new/:page" element={<InitialPage />} />
-                            <Route path="search/:book/*" element={<ResultsPage />} />
-                            <Route path="books/:isbn" element={<BookPage />} />
-                            <Route path="favourites" element={<Favourites />} />
-                            <Route path="cart" element={isLogged ? <Cart /> : <Navigate to={'/account/auth'}/>} />
-                            <Route path="account" element={<Account />}>
-                                <Route path="auth" element={<Form />}/>
-                            </Route>
-                            <Route path="*" element={<Navigate to="/new/1" />} />
-                        </Route>
-                    </Routes>
-                </Main>
-            </div>
-        </BrowserRouter>
-    );
+        <Main>
+          <Routes>
+            <Route path="/" element={<BooksPage />}>
+              <Route index element={<InitialPage />} />
+              <Route path="new/:page" element={<InitialPage />} />
+              <Route path="search/:book/*" element={<ResultsPage />} />
+              <Route path="books/:isbn" element={<BookPage />} />
+              <Route path="favourites" element={<Favourites />} />
+              {/* <Route path="cart" element={isLogged ? <Cart /> : <Navigate to={'/account/auth'}/>} /> */}
+              {/* For testing purposes only */}
+              <Route path="cart" element={<Cart />} />
+              <Route path="account" element={<Account />}>
+                <Route path="auth" element={<Form />} />
+              </Route>
+              <Route path="*" element={<Navigate to="/new/1" />} />
+            </Route>
+          </Routes>
+        </Main>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/main/bookPage/bookDetails/BookDetails.tsx
+++ src/components/main/bookPage/bookDetails/BookDetails.tsx
@@ -16,51 +16,57 @@ import { UISize } from "../../../../enums/enums";
 import s from "./BookDetails.module.scss";
 
 const BookDetails: React.FC = () => {
-    const { price, isbn13, title, subtitle, image  } = useAppSelector((state) => state.oneBook.book);
-    const preview = useAppSelector((state) => state.oneBook.book.pdf); //объект со ссылками
-    const isLogged = useAppSelector(state => state.auth.isLogged)
-    const navigate = useNavigate()
-    const isEmptyPreview = _isEmpty(preview); // проверка что не пустой
+  const { price, isbn13, title, subtitle, image } = useAppSelector(
+    (state) => state.oneBook.book
+  );
+  const preview = useAppSelector((state) => state.oneBook.book.pdf); //объект со ссылками
+  const isLogged = useAppSelector((state) => state.auth.isLogged);
+  const navigate = useNavigate();
+  const isEmptyPreview = _isEmpty(preview); // проверка что не пустой
 
-    const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-    const isValidPrice = useRef(true)
-    isValidPrice.current = !!(+price.slice(1))
+  const isValidPrice = useRef(true);
+  isValidPrice.current = !!+price.slice(1);
 
-    const preparedData = useRef<ICartBook>({})
-    preparedData.current[isbn13] = {
-        image, 
-        title, 
-        subtitle, 
-        price, 
-        isbn13
-    }
+  const preparedData = useRef<ICartBook>({});
+  preparedData.current[isbn13] = {
+    image,
+    title,
+    subtitle,
+    price,
+    isbn13,
+  };
 
-    const handleCartClick = () => {
-        if (isLogged) {
-            dispatch(addProduct(preparedData.current))
-        } else {
-            navigate('/account/auth')
-        }
-    }
+  const handleCartClick = () => {
+    // if (isLogged) {
+    //     dispatch(addProduct(preparedData.current))
+    // } else {
+    //     navigate('/account/auth')
+    // }
+    //For testing purposes only
+    dispatch(addProduct(preparedData.current));
+  };
 
-    return (
-        <div className={s.details_container}>
-            <div className={s.details}>
-                <UIBookRates cName={`_${UISize.Large}`} price={price} isbn13={isbn13} />
-                <DetailsList />
-                <MoreDetailsList />
-            </div>
-            <UIPrimaryButton 
-                text="Add to cart" 
-                cNameBtn="ui_btn_bookDetails" 
-                isValidPrice={isValidPrice.current}
-                onClick={handleCartClick}
-            />
-            {preview && !isEmptyPreview && <PreviewBook preview={Object.values(preview)[0]} />}
-            {/* если не пустой берем первую ссылку */}
-        </div>
-    );
+  return (
+    <div className={s.details_container}>
+      <div className={s.details}>
+        <UIBookRates cName={`_${UISize.Large}`} price={price} isbn13={isbn13} />
+        <DetailsList />
+        <MoreDetailsList />
+      </div>
+      <UIPrimaryButton
+        text="Add to cart"
+        cNameBtn="ui_btn_bookDetails"
+        isValidPrice={isValidPrice.current}
+        onClick={handleCartClick}
+      />
+      {preview && !isEmptyPreview && (
+        <PreviewBook preview={Object.values(preview)[0]} />
+      )}
+      {/* если не пустой берем первую ссылку */}
+    </div>
+  );
 };
 
 export default BookDetails;
